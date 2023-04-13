4fa2d60bb133fb8d8ca116a38119d218f542f897
kouunnki <123440552+kouunnki@users.noreply.github.com>
2023-04-13 15:39:28 +0900

    Update loss function.py

--- loss function.py
+++ loss function.py
@@ -1,9 +1,8 @@
 import numpy as np
 from dataprocessing import *
-#box:[上, 左, 下, 右]
 
-def DIoU(box1, box2, w_xy, angles_true, angles_pre):
-    # 计算中间矩形的宽高
+def loss_function(box1, box2, w_xy, angles_true, angles_pre):
+
     Npos = 0
     L_reg_sum = 0
     Lcls_sum = 0
@@ -22,23 +21,22 @@ def DIoU(box1, box2, w_xy, angles_true, angles_pre):
                 in_h = min(box1[i,j,3], box2[i,j,3]) - max(box1[i,j,1], box2[i,j,1])
                 in_w = min(box1[i,j,2], box2[i,j,2]) - max(box1[i,j,0], box2[i,j,0])
     
-    # 计算交集、并集面积
                 inter = 0 if in_h < 0 or in_w < 0 else in_h * in_w
                 union = (box1[i,j,3] - box1[i,j,1]) * (box1[i,j,2] - box1[i,j,0]) + \
                 (box2[i,j,3] - box2[i,j,1]) * (box2[i,j,2] - box2[i,j,0]) - inter
-    # 计算IoU
+    # calculate IoU
                 iou = inter / union
-    # 计算对角线长度
+    # calculate diagonal
                 x1,y1,x2,y2 = box1[i,j,:4] 
                 x3,y3,x4,y4 = box2[i,j,:4]
                 C = np.sqrt((max(x1,x2,x3,x4)-min(x1,x2,x3,x4))**2 + \
                 (max(y1,y2,y3,y4)-min(y1,y2,y3,y4))**2)
-    # 计算中心点间距
+    # calculate distance of center point
                 point_1 = ((x2+x1)/2, (y2+y1)/2)
                 point_2 = ((x4+x3)/2, (y4+y3)/2)
                 D = np.sqrt((point_2[0]-point_1[0])**2 + \
                 (point_2[1]-point_1[1])**2)
-    # 计算空白部分占比
+    # calculate DIOU
                 lens = D**2 / C**2
                 diou = iou - lens
 
@@ -57,7 +55,3 @@ def DIoU(box1, box2, w_xy, angles_true, angles_pre):
     L_reg = 2/Npos*L_reg_sum
     L = Lcls + L_reg
     return L
-box1 = target(points,angles_original)
-box2 = target(points,angles_original)
-L = DIoU(box1[0],box2[0],box1[1],box1[2],box2[2])
-print(L)
